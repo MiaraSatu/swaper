4a82a7bf35a8a4e7e56d78c33a0dd69f688a8962
Miara Satu <miarasatu@gmail.com>
2025-01-20 21:30:23 +0300

    add methode findBySenderOrReceiver in FriendShipRepository

--- src/main/java/com/example/swaper/repository/FriendShipRepository.java
+++ src/main/java/com/example/swaper/repository/FriendShipRepository.java
@@ -1,9 +1,13 @@
 package com.example.swaper.repository;
 
+import com.example.swaper.model.DBUser;
 import com.example.swaper.model.FriendShip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FriendShipRepository extends JpaRepository<FriendShip, Integer> {
+    public List<FriendShip> findBySenderOrReceiver(DBUser sender, DBUser receiver);
 }
