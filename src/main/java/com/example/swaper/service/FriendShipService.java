879804758ff6c33dacfebff408e679feca398d78
Miara Satu <miarasatu@gmail.com>
2025-01-20 21:31:30 +0300

    create FriendShipService and add method getFriendShipRelatedTo

---
+++ src/main/java/com/example/swaper/service/FriendShipService.java
@@ -0,0 +1,20 @@
+package com.example.swaper.service;
+
+import com.example.swaper.model.DBUser;
+import com.example.swaper.model.FriendShip;
+import com.example.swaper.repository.FriendShipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FriendShipService {
+    @Autowired
+    private FriendShipRepository friendShipRepository;
+
+    public List<FriendShip> getFriendShipRelatedTo(DBUser subject) {
+        return friendShipRepository.findBySenderOrReceiver(subject, subject);
+    }
+
+}
