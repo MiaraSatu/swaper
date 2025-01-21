4bfc16776219e3ffcb1890a7050897aa2b929354
Miara Satu <miarasatu@gmail.com>
2025-01-21 09:29:00 +0300

    add abstract method countBySenderAndReceiverOrSenderAndReceiver in MessageRepository to count message exchange based on sender and receiver

--- src/main/java/com/example/swaper/repository/MessageRepository.java
+++ src/main/java/com/example/swaper/repository/MessageRepository.java
@@ -1,9 +1,12 @@
 package com.example.swaper.repository;
 
+import com.example.swaper.model.Box;
+import com.example.swaper.model.DBUser;
 import com.example.swaper.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    long countBySenderAndReceiverOrSenderAndReceiver(DBUser user1, DBUser user2 , DBUser user3, DBUser user4); // user3 = user1 && user4 = user2
 }
