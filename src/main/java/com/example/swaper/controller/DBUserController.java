04effaae50d5acd20f6f0948ef4a9f2081c9edf3
Miara Satu <miarasatu@gmail.com>
2025-01-21 15:02:23 +0300

    create a route to searchDiscusser in DBUserController

--- src/main/java/com/example/swaper/controller/DBUserController.java
+++ src/main/java/com/example/swaper/controller/DBUserController.java
@@ -2,13 +2,16 @@ package com.example.swaper.controller;
 
 import com.example.swaper.model.DBUser;
 import com.example.swaper.service.DBUserService;
+import com.example.swaper.service.FriendShipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,5 +24,11 @@ public class DBUserController {
     public List<DBUser> getDiscussers(@AuthenticationPrincipal Jwt jwt) {
         DBUser subject = dbUserService.get(jwt.getClaim("sub"));
         return dbUserService.getMostFrequentedFriends(subject);
+    }
+
+    @GetMapping("/discussers/search")
+    public List<DBUser> searchDiscusser(@Param("kw") String kw, @AuthenticationPrincipal Jwt jwt) {
+        DBUser subject = dbUserService.get(jwt.getClaim("sub"));
+        return dbUserService.searchDiscusser(kw, subject);
     }
 }
