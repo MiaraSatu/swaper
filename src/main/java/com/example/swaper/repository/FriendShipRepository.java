55633ad7c66947a7dd2becf90a8f973309006500
Miara Satu <miarasatu@gmail.com>
2025-01-21 14:56:08 +0300

    create a JPQL query attached to searchByAUserName to search a friendShip by a username in FriendShipRepository

--- src/main/java/com/example/swaper/repository/FriendShipRepository.java
+++ src/main/java/com/example/swaper/repository/FriendShipRepository.java
@@ -3,6 +3,8 @@ package com.example.swaper.repository;
 import com.example.swaper.model.DBUser;
 import com.example.swaper.model.FriendShip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 @Repository
 public interface FriendShipRepository extends JpaRepository<FriendShip, Integer> {
     public List<FriendShip> findBySenderOrReceiver(DBUser sender, DBUser receiver);
+
+    @Query("SELECT f FROM FriendShip f JOIN DBUser u ON (f.sender = u AND f.receiver = :subject)"+
+        "OR (f.receiver = u AND f.sender = :subject) WHERE u.name LIKE CONCAT('%', :kw, '%')"
+    )
+    public List<FriendShip> searchByUserName(@Param("kw") String kw, @Param("subject") DBUser subject);
 }
