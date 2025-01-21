f43fef2999a6f8fd80b6cb4ea959ba80b3a67457
Miara Satu <miarasatu@gmail.com>
2025-01-21 09:31:40 +0300

    enable DBUserService::getMostFrequentedFriends [without date interal]

--- src/main/java/com/example/swaper/service/DBUserService.java
+++ src/main/java/com/example/swaper/service/DBUserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class DBUserService {
@@ -15,6 +16,9 @@ public class DBUserService {
 
     @Autowired
     private FriendShipService friendShipService;
+
+    @Autowired
+    private MessageService messageService;
 
     public void add(DBUser dbUser) {
         userRepo.save(dbUser);
@@ -30,6 +34,22 @@ public class DBUserService {
 
     public DBUser get(int id) {
         return userRepo.findById(id).get();
+    }
+
+    public List<DBUser> getMostFrequentedFriends(DBUser subject) {
+        List<DBUser> friends = this.getFriends(subject);
+        List<DBUser> frequentedFriends;
+        Map<DBUser, Long> messageStat = new HashMap<>();
+        for(DBUser friend: friends) {
+            messageStat.put(friend, messageService.countMessageExchanged(subject, friend));
+        }
+        frequentedFriends = messageStat.entrySet()
+                .stream()
+                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+                .map(Map.Entry::getKey)
+                .limit(10)
+                .toList();
+        return frequentedFriends;
     }
 
     public List<DBUser> getFriends(DBUser subject) {
