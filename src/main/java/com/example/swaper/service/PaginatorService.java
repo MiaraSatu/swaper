19b8c97face1c909f5ce8e9a7847f8b9d31a2648
Miara Satu <miarasatu@gmail.com>
2025-02-10 16:12:56 +0300

    set seeMoreUrl null if nextUrl is not available

--- src/main/java/com/example/swaper/service/PaginatorService.java
+++ src/main/java/com/example/swaper/service/PaginatorService.java
@@ -9,12 +9,17 @@ import java.util.Map;
 @Service
 public class PaginatorService<T> {
     public Map<String, Object> paginate(List<T> baseList, String baseUrl, Integer page, long limit) {
+        int size = baseList.size();
+        int availablePage = (int)((size - 1) / limit) + 1;
         page = (page == null || page == 0) ? 1 : page;
+        if(page > availablePage) {
+            page = availablePage;
+        }
         long offset = (page - 1) * limit;
         List<T> data = baseList.stream().skip(offset).limit(limit).toList();
         Map<String, Object> response = new HashMap<>();
         response.put("data", data);
-        response.put("seeMoreUrl", baseUrl+"?page="+(page+1));
+        response.put("seeMoreUrl", (page < availablePage) ? baseUrl+"?page="+(page+1) : null);
         return response;
     }
 }
